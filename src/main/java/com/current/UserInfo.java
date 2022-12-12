d99abc630b2731ff488537cd16929b181888fab2
NehaUpadhyay25 <nxu3128@rit.edu>
2022-12-12 00:27:03 -0500

    test changes

--- src/main/java/com/current/UserInfo.java
+++ src/main/java/com/current/UserInfo.java
@@ -4,7 +4,6 @@ package com.current;
  * This is a Plain Old Java Object (POJO) to hold data that we'd like to pass around in our program.
  */
 public class UserInfo {
-
     private final String name;
     private final String address;
 
